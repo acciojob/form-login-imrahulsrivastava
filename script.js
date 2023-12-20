703d9ceca724d0a5a3bbc91f26ad7bbeb5122247
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-12-20 12:52:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
-    //Write your code here
+// Write your script here
 
+function getFormvalue() {
+  const form = document.getElementById("form1");
+  const firstName = form.elements.fname.value;
+  const lastName = form.elements.lname.value;
+  alert(firstName + " " + lastName);
 }
